9a3a4bf2065befa21c00aeacff2e0ed5fed10597
kev <kev@flujos.org>
2012-07-30 16:20:43 -0500

    subida comprobado

--- nginx/upload.rb
+++ nginx/upload.rb
@@ -8,13 +8,13 @@ require 'sinatra'
 require 'fileutils'
 require 'data_mapper'
 
-html_path = '/srv/http/rockola/files' #hard links to /srv/media
+html_path = '/tmp/http/rockola/files/' #hard links to /srv/media
 #html_path = '/srv/http/rockola/files/$user' caundo tenemos sessiones
-store_path = '/srv/media' #git-media store media files
+store_path = '/tmp/media' #git-media store media files
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/rock.db")
 
-class File 
+class Media
   include DataMapper::Resource
   property :id,           Serial
   property :name,         String, :required => true
@@ -22,7 +22,7 @@ class File
   property :type,         String, :required => true
   property :path,         String, :required => true
   property :size,         String, :required => true
-  property :user,         String, :required => true
+#  property :user,         String, :required => true
   property :completed_at, DateTime
 end
 DataMapper.finalize
@@ -32,19 +32,26 @@ post '/upload' do
    md5 = params['file.md5']
    name = params['file.name']
    type = params['file.content_type']
-   path = params['file.path']  
+   path = params['file.path']
    size = params['file.size']
    submit = params['submit']
-   
+
+  # imprimir valores (debug) 
+  puts "{ name: #{name}, path: #{path}, type: #{type}, md5: #{md5}, size: #{size}, submit: #{submit} }"
+  store = store_path + "/" + md5
+
+  if File.exists? store 
+    'File Exists'
+
+  else
   # imitar git-media
-  FileUtils.cp("#{path}", "#{store_path}/#{md5}")
-  FileUtils.ln("#{store_path}/#{md5}", "#{html_path}/#{name}")
+  FileUtils.cp("#{path}", store)
+  FileUtils.ln(store, "#{html_path}/#{name}")
   
-  path = "#{html_path}/#{name}"
-  File.create(name: name, md5: md5, type: type, path: path, size: size, user: user)
-  
-   # imprimir valores (debug) 
-   #puts "{ name: #{name}, path: #{path}, type: #{type}, md5: #{md5}, size: #{size}, submit: #{submit} }"
+  finpath = "#{html_path}/#{name}"
+  #File.create(name: name, md5: md5, type: type, path: path, size: size, user: user)
+  Media.create(name: name, md5: md5, type: type, path: finpath, size: size)
+  end
 
 end
 
