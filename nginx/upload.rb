c015d517af2b607e458b00b3579794f819b85e39
kev <kev@flujos.org>
2012-08-02 23:26:50 -0500

    require login to upload

--- nginx/upload.rb
+++ nginx/upload.rb
@@ -7,11 +7,20 @@
 require 'sinatra'
 require 'fileutils'
 require 'data_mapper'
+require 'sinatra/flash'
 
-html_path = '/tmp/http/rockola/files' #hard links to /srv/media
+
+enable :sessions
+set :username,'Bond'
+set :token,'shakenN0tstirr3d'
+set :password,'007'
+set :session_secret, "A1 sauce 1s so good you should use 1t on a11 yr st34ksssss"
+#set :dump_errors, false
+set :html_path, '/tmp/http/rockola/files' #hard links to /srv/media
 #html_path = '/srv/http/rockola/files/$user' caundo tenemos sessiones
-store_path = '/tmp/media' #git-media store media files
+set :store_path, '/tmp/media' #git-media store media files
 
+# inicializar base de datos
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/rock.db")
 
 class Media
@@ -28,43 +37,73 @@ end
 DataMapper.finalize
 Media.auto_upgrade!
 
-post '/upload' do
-
-   md5 = params['file.md5']
-   name = params['file.name']
-   type = params['file.content_type']
-   path = params['file.path']
-   size = params['file.size']
-   submit = params['submit']
-
-  # imprimir valores (debug) 
-  puts "{ name: #{name}, path: #{path}, type: #{type}, md5: #{md5}, size: #{size}, submit: #{submit} }"
-
-  store = store_path + "/" + md5
-
-  if File.exists? store 
-    'File Exists'
-  else
-  # imitar git-media
-  FileUtils.cp("#{path}", store)
-  FileUtils.ln(store, "#{html_path}/#{name}")
-  
-  finpath = "#{html_path}/#{name}"
-  #File.create(name: name, md5: md5, type: type, path: path, size: size, user: user)
-  Media.create(name: name, md5: md5, type: type, path: finpath, size: size)
-  end
-
+helpers do
+  def admin? ; request.cookies[settings.username] == settings.token ; end
+  def protected! ; halt [ 401, 'Not Authorized' ] unless admin? ; end
+  def get_params(md5) ; return Media.all(:md5 => md5); end
 end
 
+post '/upload' do
+
+   unless admin?
+      set :params,  params
+      redirect "/login"
+   end
+   if defined? params['file.md5']
+     md5 = params['file.md5']
+     name = params['file.name']
+     type = params['file.content_type']
+     path = params['file.path']
+     size = params['file.size']
+     submit = params['submit']
+   end
+
+   store = settings.store_path + "/" + md5
+   if File.exists? store 
+     flash[:upload] = 'File Exists'
+     redirect to("/media/#{md5}")
+   end
+   user = request.cookies[settings.username]
+   finpath = settings.html_path + "/" + name
+   FileUtils.cp(path, store)
+   FileUtils.ln(store, finpath)
+   #Media.create(params)
+   Media.create(name: name, md5: md5, type: type, path: finpath, size: size, user: user)
+   flash[:upload] = 'New file: ' + name
+   redirect to("/media/#{md5}")
+
+   response.set_cookie(settings.params, false)
+   redirect to("/media/#{md5}")
+end
 get '/media' do
-    # get the latest 20 posts
     @media = Media.all(:order => [ :id.desc ], :limit => 20)
     erb :media_html
 end
-
-get '/media_json' do
-    # get the latest 20 posts
+get '/media/:md5' do
+    @media = get_params(params[:md5])
+    erb :media_html
+end
+get '/media/json' do # ¿porque no funciona?
     @media = Media.all(:order => [ :id.desc ], :limit => 20)
-    erb :media_json
+    erb :media_json, :layout => false
+end
+get '/media/:md5/:json' do
+    @media = get_params(params[:md5])
+    erb :media_json, :layout => false
+end
 
+get '/logout' do 
+  response.set_cookie(settings.username, false)
+  redirect '/' 
+end
+get('/login'){ haml :admin }
+post '/login' do
+  if params['username']==settings.username&&params['password']==settings.password
+    response.set_cookie(settings.username,settings.token) 
+    redirect to("/")
+  else
+    "Username or Password incorrect"
+  end
+
+  #response.set_cookie("origin", :value => "/upload", :domain => "")
 end
