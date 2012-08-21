741c90545fb8806a56ce6551ee0295d1f1d08bbc
kev <kev@flujos.org>
2012-08-20 19:27:27 -0500

    mas facil leer

--- nginx/mktmp.sh
+++ nginx/mktmp.sh
@@ -1,4 +1,4 @@
 mkdir -p /tmp/http/rockola/files
 mkdir -p /tmp/media
 mkdir /tmp/{0,1,2,3,4,5,6,7,8,9}
-chown http /tmp/0 /tmp/1 /tmp/2 /tmp/3 /tmp/4 /tmp/5 /tmp/6 /tmp/7 /tmp/8 /tmp/9
+chown http /tmp/{0,1,2,3,4,5,6,7,8,9}
